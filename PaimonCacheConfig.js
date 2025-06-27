148bcd273c4d78fb4b93b8dbd64b3e96a3008fe0
flozwer <92696432+Fl0zWer@users.noreply.github.com>
2025-06-27 16:13:39 -0300

    Update server configurations

--- PaimonCacheConfig.js
+++ PaimonCacheConfig.js
@@ -1,8 +1,8 @@
 export const serverConfigs = {
     "1215038551755067442": {
         "server_id": "1215038551755067442",
-        "server_name": "",
-        "stage_channel_id": null,
+        "server_name": "\ud835\udc0f\ud835\udc1a\ud835\udc22\ud835\udc26\ud835\udc28\ud835\udc27\ud835\udc0b\ud835\udc1a\ud835\udc27\ud835\udc1d\ud835\udc22\ud835\udc1a",
+        "stage_channel_id": 1215040218495852544,
         "admin_roles": [],
         "bot_permissions": {
             "accept_levels": [
@@ -22,6 +22,6 @@ export const serverConfigs = {
             "max_queue_size": 50
         },
         "created_at": "2025-06-27 19:13:17.261507",
-        "last_updated": "2025-06-27 19:13:17.261520"
+        "last_updated": "2025-06-27 19:13:38.784921"
     }
 };
