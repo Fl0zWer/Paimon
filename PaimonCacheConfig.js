8238f63fd9a0dfb06b6133acd6113a2e8cfd1393
flozwer <92696432+Fl0zWer@users.noreply.github.com>
2025-06-27 16:13:17 -0300

    Update server configurations

--- PaimonCacheConfig.js
+++ PaimonCacheConfig.js
@@ -1,1 +1,27 @@
-
+export const serverConfigs = {
+    "1215038551755067442": {
+        "server_id": "1215038551755067442",
+        "server_name": "",
+        "stage_channel_id": null,
+        "admin_roles": [],
+        "bot_permissions": {
+            "accept_levels": [
+                "administrator"
+            ],
+            "stage_management": [
+                "administrator"
+            ],
+            "config_management": [
+                "administrator"
+            ]
+        },
+        "custom_settings": {
+            "auto_dm_on_accept": true,
+            "auto_dm_on_reject": true,
+            "require_video_for_demons": true,
+            "max_queue_size": 50
+        },
+        "created_at": "2025-06-27 19:13:17.261507",
+        "last_updated": "2025-06-27 19:13:17.261520"
+    }
+};
