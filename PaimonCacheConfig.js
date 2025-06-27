f59bff8d96fe00d73c07e1dc75731f5f611ed720
flozwer <92696432+Fl0zWer@users.noreply.github.com>
2025-06-27 19:24:11 -0300

    Actualización automática de configuración - 2025-06-27 22:24:11

--- PaimonCacheConfig.js
+++ PaimonCacheConfig.js
@@ -1,27 +1,42 @@
-export const serverConfigs = {
-    "1215038551755067442": {
-        "server_id": "1215038551755067442",
-        "server_name": "\ud835\udc0f\ud835\udc1a\ud835\udc22\ud835\udc26\ud835\udc28\ud835\udc27\ud835\udc0b\ud835\udc1a\ud835\udc27\ud835\udc1d\ud835\udc22\ud835\udc1a",
-        "stage_channel_id": 1215040218495852544,
-        "admin_roles": [],
-        "bot_permissions": {
-            "accept_levels": [
-                "administrator"
-            ],
-            "stage_management": [
-                "administrator"
-            ],
-            "config_management": [
-                "administrator"
-            ]
-        },
-        "custom_settings": {
-            "auto_dm_on_accept": true,
-            "auto_dm_on_reject": true,
-            "require_video_for_demons": true,
-            "max_queue_size": 50
-        },
-        "created_at": "2025-06-27 19:13:17.261507",
-        "last_updated": "2025-06-27 19:13:38.784921"
-    }
+const configs = {
+  "1215038551755067442": {
+    "server_id": "1215038551755067442",
+    "server_name": "",
+    "stage_channel_id": null,
+    "stage_channel_name": null,
+    "level_channel_id": 1388274684574830812,
+    "level_channel_name": "niveles",
+    "admin_roles": [],
+    "bot_permissions": {
+      "accept_levels": [
+        "administrator"
+      ],
+      "stage_management": [
+        "administrator"
+      ],
+      "config_management": [
+        "administrator"
+      ]
+    },
+    "custom_settings": {
+      "auto_dm_on_accept": true,
+      "auto_dm_on_reject": true,
+      "require_video_for_demons": true,
+      "max_queue_size": 50
+    },
+    "leveling_settings": {
+      "base_xp": 5,
+      "xp_cooldown_minutes": 3,
+      "level_roles": {},
+      "top_roles": {}
+    },
+    "anti_raid_settings": {
+      "enabled": true,
+      "spam_threshold": 8,
+      "time_window": 10,
+      "max_warnings": 3
+    },
+    "created_at": "2025-06-27 22:11:20.315950",
+    "last_updated": "2025-06-27 22:24:10.634349"
+  }
 };
