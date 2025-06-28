b70b7ee547f2ce6a3b0230b87fcabd84f5a517aa
flozwer <92696432+Fl0zWer@users.noreply.github.com>
2025-06-28 16:10:14 -0300

    Actualización automática de configuración - 2025-06-28 19:10:13

--- PaimonCacheConfig.js
+++ PaimonCacheConfig.js
@@ -37,8 +37,374 @@ const configs = {
       "max_warnings": 3
     },
     "created_at": "2025-06-27 22:11:20.315950",
-    "last_updated": "2025-06-28 04:01:24.682069",
+    "last_updated": "2025-06-28 19:10:13.449672",
     "ticket_channel_id": 1388296577294270626,
-    "ticket_channel_name": "tickets"
+    "ticket_channel_name": "tickets",
+    "tts_channel_id": null,
+    "tts_channel_name": null,
+    "music_channel_id": null,
+    "music_channel_name": null,
+    "welcome_channel_id": null,
+    "welcome_channel_name": null,
+    "tts_settings": {
+      "enabled": true,
+      "language": "es",
+      "speed": 1.0,
+      "volume": 0.8,
+      "engine": "pyttsx3",
+      "auto_join": false,
+      "paimon_mode": false
+    },
+    "music_settings": {
+      "enabled": true,
+      "default_volume": 0.5,
+      "max_queue_size": 20,
+      "auto_disconnect": true,
+      "disconnect_timeout": 300,
+      "loop_mode": "none"
+    },
+    "welcome_settings": {
+      "enabled": true,
+      "message": "¡Bienvenido {user} al servidor!",
+      "embed_color": "#00ff00",
+      "dm_welcome": false,
+      "role_on_join": null
+    },
+    "goodbye_settings": {
+      "enabled": true,
+      "message": "¡Adiós {user}! Esperamos verte pronto.",
+      "embed_color": "#ff0000",
+      "log_leaves": true
+    },
+    "moderation_settings": {
+      "auto_delete_invites": true,
+      "auto_delete_spam": true,
+      "warn_on_links": false,
+      "mute_duration": 300,
+      "log_mod_actions": true
+    },
+    "complete_backup": {
+      "server_config": {
+        "server_id": "1215038551755067442",
+        "server_name": "",
+        "stage_channel_id": null,
+        "stage_channel_name": null,
+        "level_channel_id": 1360012235640147978,
+        "level_channel_name": "『✦📊⋆【𝐋𝐞𝐯𝐞𝐥-𝐮𝐩】⋆✦』",
+        "admin_roles": [],
+        "bot_permissions": {
+          "accept_levels": [
+            "administrator"
+          ],
+          "stage_management": [
+            "administrator"
+          ],
+          "config_management": [
+            "administrator"
+          ]
+        },
+        "custom_settings": {
+          "auto_dm_on_accept": true,
+          "auto_dm_on_reject": true,
+          "require_video_for_demons": true,
+          "max_queue_size": 50
+        },
+        "leveling_settings": {
+          "base_xp": 5,
+          "xp_cooldown_minutes": 3,
+          "level_roles": {},
+          "top_roles": {}
+        },
+        "anti_raid_settings": {
+          "enabled": true,
+          "spam_threshold": 8,
+          "time_window": 10,
+          "max_warnings": 3
+        },
+        "created_at": "2025-06-27 22:11:20.315950",
+        "last_updated": "2025-06-28 04:01:24.682069",
+        "ticket_channel_id": 1388296577294270626,
+        "ticket_channel_name": "tickets",
+        "tts_channel_id": null,
+        "tts_channel_name": null,
+        "music_channel_id": null,
+        "music_channel_name": null,
+        "welcome_channel_id": null,
+        "welcome_channel_name": null,
+        "tts_settings": {
+          "enabled": true,
+          "language": "es",
+          "speed": 1.0,
+          "volume": 0.8,
+          "engine": "pyttsx3",
+          "auto_join": false,
+          "paimon_mode": false
+        },
+        "music_settings": {
+          "enabled": true,
+          "default_volume": 0.5,
+          "max_queue_size": 20,
+          "auto_disconnect": true,
+          "disconnect_timeout": 300,
+          "loop_mode": "none"
+        },
+        "welcome_settings": {
+          "enabled": true,
+          "message": "¡Bienvenido {user} al servidor!",
+          "embed_color": "#00ff00",
+          "dm_welcome": false,
+          "role_on_join": null
+        },
+        "goodbye_settings": {
+          "enabled": true,
+          "message": "¡Adiós {user}! Esperamos verte pronto.",
+          "embed_color": "#ff0000",
+          "log_leaves": true
+        },
+        "moderation_settings": {
+          "auto_delete_invites": true,
+          "auto_delete_spam": true,
+          "warn_on_links": false,
+          "mute_duration": 300,
+          "log_mod_actions": true
+        }
+      },
+      "leveling_config": {
+        "base_points": 5,
+        "xp_cooldown": 180,
+        "level_roles": [],
+        "user_levels": {
+          "1211030300642119813": {
+            "points": 5,
+            "level": 0,
+            "daily_streak": 1,
+            "total_messages": 1
+          },
+          "1024467029358542978": {
+            "points": 10,
+            "level": 0,
+            "daily_streak": 1,
+            "total_messages": 2
+          },
+          "839600325312643072": {
+            "points": 10,
+            "level": 0,
+            "daily_streak": 1,
+            "total_messages": 2
+          },
+          "1316518623119933452": {
+            "points": 5,
+            "level": 0,
+            "daily_streak": 1,
+            "total_messages": 1
+          },
+          "1119831848235839558": {
+            "points": 5,
+            "level": 0,
+            "daily_streak": 1,
+            "total_messages": 1
+          },
+          "1324871450053709866": {
+            "points": 5,
+            "level": 0,
+            "daily_streak": 1,
+            "total_messages": 1
+          },
+          "362821815527276545": {
+            "points": 5,
+            "level": 0,
+            "daily_streak": 1,
+            "total_messages": 1
+          },
+          "1321990949450289194": {
+            "points": 30,
+            "level": 0,
+            "daily_streak": 1,
+            "total_messages": 6
+          },
+          "1185324802962100275": {
+            "points": 15,
+            "level": 0,
+            "daily_streak": 2,
+            "total_messages": 3
+          },
+          "517514609721344030": {
+            "points": 120,
+            "level": 1,
+            "daily_streak": 1,
+            "total_messages": 24
+          },
+          "672496337006362634": {
+            "points": 585,
+            "level": 2,
+            "daily_streak": 2,
+            "total_messages": 117
+          }
+        }
+      },
+      "music_config": {
+        "queues": [],
+        "volumes": 0.5,
+        "loops": "none",
+        "now_playing": null
+      },
+      "tts_config": {
+        "enabled": true,
+        "language": "es",
+        "speed": 1.0,
+        "volume": 0.8,
+        "engine": "pyttsx3",
+        "voice_channels": null,
+        "paimon_sessions": null,
+        "paimon_queue": []
+      },
+      "stream_config": {
+        "notifications": {},
+        "active_streams": {}
+      },
+      "nekotina_config": {
+        "welcome_messages": {},
+        "goodbye_messages": {},
+        "roleplay_counts": {},
+        "economy_data": {},
+        "economy_shop": {},
+        "economy_pets": {},
+        "economy_marriages": {},
+        "starboard_config": {},
+        "starboard_messages": {},
+        "giveaways": {},
+        "birthday_config": {},
+        "birthday_users": {},
+        "waifu_system": {},
+        "anime_tracking": {},
+        "protection_config": {},
+        "logging_config": {}
+      },
+      "anti_raid_config": {
+        "spam_threshold": 8,
+        "time_window": 10,
+        "max_warnings": 3,
+        "user_message_count": {
+          "1308256595825918022": [
+            1751137802.518683
+          ],
+          "1101311707244142642": [
+            1751137805.643877
+          ],
+          "672496337006362634": [
+            1751137812.6875973
+          ]
+        },
+        "user_warning_count": {
+          "1308256595825918022": 0,
+          "1101311707244142642": 0,
+          "672496337006362634": 0
+        }
+      },
+      "level_queue_config": {
+        "queue": [],
+        "current_level": null,
+        "banned_ids": [],
+        "cooldown_seconds": 0,
+        "last_request_time": {},
+        "official_channel_id": null,
+        "dm_sent_users": []
+      },
+      "flozwer_notifications": {
+        "error": "FlozWer module not available"
+      },
+      "local_files_config": {
+        "js_file_content": {
+          "2": {
+            "id": "118678388",
+            "creador": "FlozWer",
+            "usuario": "FlozWer",
+            "dificultad": "Harder",
+            "tipo": "classic",
+            "url": "",
+            "estrellas": "0",
+            "imagen": null,
+            "video": null,
+            "descargas": "155",
+            "likes": "30",
+            "length": "XL",
+            "orbs": "0",
+            "descripcion": "un layout hecho por las prisas",
+            "song": "Kisaragi",
+            "song_artist": "Camellia",
+            "song_id": "10007168",
+            "song_size": "6.68MB",
+            "discord_user_id": 672496337006362634
+          },
+          "3": {
+            "id": "120512937",
+            "creador": "FlozWer",
+            "usuario": "FlozWer",
+            "dificultad": "Harder",
+            "tipo": "classic",
+            "url": "",
+            "estrellas": "0",
+            "imagen": null,
+            "video": null,
+            "descargas": "551",
+            "likes": "43",
+            "length": "Long",
+            "orbs": "0",
+            "descripcion": "porfin arregle el problema del unlisted 8)",
+            "song": "1991 REEBOK PUMPS (demo)",
+            "song_artist": "SpeedoSausage",
+            "song_id": "759834",
+            "song_size": "7MB",
+            "discord_user_id": 672496337006362634
+          },
+          "4": {
+            "id": "120235465",
+            "creador": "FlozWer",
+            "usuario": "FlozWer",
+            "dificultad": "Harder",
+            "tipo": "classic",
+            "url": "",
+            "estrellas": "6",
+            "imagen": null,
+            "video": null,
+            "descargas": "20,381",
+            "likes": "1,698",
+            "length": "Long",
+            "orbs": "225",
+            "descripcion": "Nose, guiso, proximamente todas las dedicaciones (nose como salen en gd) XD",
+            "song": "something new",
+            "song_artist": "notxaev",
+            "song_id": "1251688",
+            "song_size": "12.81MB",
+            "discord_user_id": 672496337006362634
+          }
+        },
+        "stage_channel_file": "1215040218495852544",
+        "last_channel_file": null
+      },
+      "global_config": {
+        "base_points": 5,
+        "xp_cooldown": 180,
+        "spam_threshold": 8,
+        "time_window": 10,
+        "max_warnings": 3,
+        "tts_enabled": true,
+        "tts_language": "es",
+        "tts_speed": 1.0,
+        "tts_volume": 0.8,
+        "tts_engine": "pyttsx3"
+      },
+      "sync_metadata": {
+        "sync_timestamp": "2025-06-28T19:10:13.305341",
+        "guild_id": 1215038551755067442,
+        "guild_name": "𝐏𝐚𝐢𝐦𝐨𝐧𝐋𝐚𝐧𝐝𝐢𝐚",
+        "synced_by": 672496337006362634,
+        "synced_by_name": "flozwer",
+        "bot_version": "1.5.50 Beta",
+        "total_configs": 11
+      }
+    },
+    "last_full_sync": "2025-06-28T19:10:13.305361",
+    "sync_performed_by": 672496337006362634
   }
 };
