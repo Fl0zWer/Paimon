a34389a6bf1c47f66be9ff273881852f7969816e
flozwer <92696432+Fl0zWer@users.noreply.github.com>
2025-06-30 12:45:57 -0300

    Add level 118509879 to all levels

--- All levels.js
+++ All levels.js
@@ -1,1 +1,22 @@
-
+export const levels = [
+  {
+    "id": "118509879",
+    "name": "Skeletal Shenanigans",
+    "creator": "YoReid",
+    "difficulty": "Medium Demon",
+    "stars": "10",
+    "downloads": "16665671",
+    "likes": "959350",
+    "song": "Slash Inferno",
+    "length": "XL",
+    "url": "https://gdbrowser.com/118509879",
+    "requested_by": {
+      "user_id": 672496337006362634,
+      "username": "flozwer",
+      "display_name": "FlozWer"
+    },
+    "requested_at": "2025-06-30T15:45:57.196096",
+    "server_id": 1215038551755067442,
+    "server_name": "𝐏𝐚𝐢𝐦𝐨𝐧𝐋𝐚𝐧𝐝𝐢𝐚"
+  }
+];
