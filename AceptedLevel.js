f700a9963402a8bce723d4f596001fb0bd7145e3
flozwer <92696432+Fl0zWer@users.noreply.github.com>
2025-06-30 12:46:32 -0300

    Add accepted level 118509879

--- AceptedLevel.js
+++ AceptedLevel.js
@@ -1,1 +1,22 @@
-
+export const levels = [
+  {
+    "id": "118509879",
+    "name": "Unknown Level",
+    "creator": "YoReid",
+    "difficulty": "Medium Demon",
+    "stars": "10",
+    "downloads": "16665671",
+    "likes": "959350",
+    "importance": 80,
+    "accepted_by": {
+      "user_id": 672496337006362634,
+      "username": "flozwer",
+      "display_name": "FlozWer"
+    },
+    "accepted_at": "2025-06-30T15:46:32.119471",
+    "unique_id": 8,
+    "url": "",
+    "song": "Slash Inferno",
+    "length": "XL"
+  }
+];
