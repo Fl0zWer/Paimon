6b9dbb13c950b034cb11cb2cacbed4a1fac6a5de
flozwer <92696432+Fl0zWer@users.noreply.github.com>
2025-07-02 13:59:52 -0300

    Add level 116791791 to all levels (Unique ID: 1)

--- All levels.js
+++ All levels.js
@@ -18,5 +18,28 @@ export const levels = [
     "requested_at": "2025-06-30T15:45:57.196096",
     "server_id": 1215038551755067442,
     "server_name": "𝐏𝐚𝐢𝐦𝐨𝐧𝐋𝐚𝐧𝐝𝐢𝐚"
+  },
+  {
+    "id": "116791791",
+    "name": "GD GANGSTER RAP",
+    "creator": "BoomKitty",
+    "difficulty": "Insano",
+    "stars": "8",
+    "downloads": "8265184",
+    "likes": "453667",
+    "song": "GEOMETRY DASH GANGSTER RAP",
+    "length": "XL",
+    "url": "https://gdbrowser.com/116791791",
+    "requested_by": {
+      "user_id": 672496337006362634,
+      "username": "flozwer",
+      "display_name": "FlozWer",
+      "platform": "Discord"
+    },
+    "requested_at": "2025-07-02T16:59:52.326575",
+    "server_id": 1215038551755067442,
+    "server_name": "𝐏𝐚𝐢𝐦𝐨𝐧𝐋𝐚𝐧𝐝𝐢𝐚",
+    "platform": "Discord",
+    "all_levels_unique_id": 1
   }
 ];
