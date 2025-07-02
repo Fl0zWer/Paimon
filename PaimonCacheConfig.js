b1833c3e29cb8be1bc88b835893c99465d573286
flozwer <92696432+Fl0zWer@users.noreply.github.com>
2025-07-02 17:23:50 -0300

    Actualización automática de configuración - 2025-07-02 20:23:50

--- PaimonCacheConfig.js
+++ PaimonCacheConfig.js
@@ -1,108 +1,4 @@
 const configs = {
-  "997345897946021928": {
-    "server_id": "997345897946021928",
-    "server_name": "",
-    "stage_channel_id": null,
-    "stage_channel_name": null,
-    "level_channel_id": 1389440797610479758,
-    "level_channel_name": "paimon-niveles",
-    "ticket_channel_id": null,
-    "ticket_channel_name": null,
-    "tts_channel_id": null,
-    "tts_channel_name": null,
-    "music_channel_id": null,
-    "music_channel_name": null,
-    "welcome_channel_id": null,
-    "welcome_channel_name": null,
-    "admin_roles": [],
-    "bot_permissions": {
-      "accept_levels": [
-        "administrator"
-      ],
-      "stage_management": [
-        "administrator"
-      ],
-      "config_management": [
-        "administrator"
-      ],
-      "moderation": [
-        "administrator"
-      ],
-      "music_management": [
-        "administrator"
-      ]
-    },
-    "custom_settings": {
-      "auto_dm_on_accept": true,
-      "auto_dm_on_reject": true,
-      "require_video_for_demons": true,
-      "max_queue_size": 50,
-      "enable_ticket_system": true,
-      "ticket_cooldown_hours": 24,
-      "enable_welcome_system": true,
-      "enable_goodbye_system": true,
-      "enable_auto_moderation": true,
-      "enable_music_system": true,
-      "enable_tts_system": true
-    },
-    "leveling_settings": {
-      "base_xp": 5,
-      "xp_cooldown_minutes": 3,
-      "level_roles": {},
-      "top_roles": {},
-      "xp_multiplier": 1.0,
-      "max_daily_xp": 1000
-    },
-    "anti_raid_settings": {
-      "enabled": true,
-      "spam_threshold": 8,
-      "time_window": 10,
-      "max_warnings": 3,
-      "ban_duration_days": 7,
-      "appeal_message": "Has sido baneado por actividad sospechosa. Puedes apelar tu baneo creando un ticket.",
-      "auto_ban": true,
-      "log_actions": true
-    },
-    "tts_settings": {
-      "enabled": true,
-      "language": "es",
-      "speed": 1.0,
-      "volume": 0.8,
-      "engine": "pyttsx3",
-      "auto_join": false,
-      "paimon_mode": false
-    },
-    "music_settings": {
-      "enabled": true,
-      "default_volume": 0.5,
-      "max_queue_size": 20,
-      "auto_disconnect": true,
-      "disconnect_timeout": 300,
-      "loop_mode": "none"
-    },
-    "welcome_settings": {
-      "enabled": true,
-      "message": "¡Bienvenido {user} al servidor!",
-      "embed_color": "#00ff00",
-      "dm_welcome": false,
-      "role_on_join": null
-    },
-    "goodbye_settings": {
-      "enabled": true,
-      "message": "¡Adiós {user}! Esperamos verte pronto.",
-      "embed_color": "#ff0000",
-      "log_leaves": true
-    },
-    "moderation_settings": {
-      "auto_delete_invites": true,
-      "auto_delete_spam": true,
-      "warn_on_links": false,
-      "mute_duration": 300,
-      "log_mod_actions": true
-    },
-    "created_at": "2025-07-01 03:02:12.995282",
-    "last_updated": "2025-07-01 03:02:13.254966"
-  },
   "1215038551755067442": {
     "server_id": "1215038551755067442",
     "server_name": "𝐏𝐚𝐢𝐦𝐨𝐧𝐋𝐚𝐧𝐝𝐢𝐚",
@@ -141,7 +37,7 @@ const configs = {
       "max_warnings": 3
     },
     "created_at": "2025-06-27 22:11:20.315950",
-    "last_updated": "2025-07-02 17:00:15.370133",
+    "last_updated": "2025-07-02 20:23:49.378022",
     "ticket_channel_id": 1388296577294270626,
     "ticket_channel_name": "tickets",
     "tts_channel_id": null,
